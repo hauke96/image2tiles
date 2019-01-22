83727b215408c39a0bc518aa018be98edf88b554
Hauke Stieler <mail@hauke-stieler.de>
2019-01-22 23:20:55 +0100

    Extract saving of image into function

--- image2tiles.cpp
+++ image2tiles.cpp
@@ -28,33 +28,27 @@ calc_overflow(cv::Mat img, cv::Rect roi, overflow_t *flow)
 	flow->left = roi.x < 0 ? -roi.x : 0;
 	flow->right = roi.x + roi.width > img.size().width ? roi.x + roi.width - img.size().width : 0;
 
-	dlog("overflows - top:%d, buttom:%d, left:%d, right:%d", flow.top, flow.bottom, flow.left, flow.right);
+	dlog("overflows - top:%d, buttom:%d, left:%d, right:%d", flow->top, flow->bottom, flow->left, flow->right);
 }
 
 void
-crop_roi(cv::Rect roi, overflow_t *flow)
+crop_roi(cv::Rect *roi, overflow_t *flow)
 {
-	roi.x += flow.left;
-	roi.y += flow.top;
-	roi.width -= flow.right + flow.left;
-	roi.height -= flow.bottom + flow.top;
+	roi->x += flow->left;
+	roi->y += flow->top;
+	roi->width -= flow->right + flow->left;
+	roi->height -= flow->bottom + flow->top;
 
-	dlog("roi - x:%d, y:%d, width:%d, height:%d", roi.x, roi.y, roi.width, roi.height);
+	dlog("roi - x:%d, y:%d, width:%d, height:%d", roi->x, roi->y, roi->width, roi->height);
 }
 
 void
-crop (cv::Mat img, cv::Rect roi, int x, int y, int z)
+crop (cv::Mat img, cv::Rect roi, cv::Mat *cropped_img)
 {
-	static int i = 0;
-
-	dlog("Crop x:%d, y:%d ...", x, y);
-
-	cv::Mat background(roi.width, roi.height, CV_8UC4, cv::Scalar(0, 0, 0, 0));
-
 	overflow_t flow;
 	calc_overflow(img, roi, &flow);
 
-	crop_roi(roi, *flow);
+	crop_roi(&roi, &flow);
 
 	// Crop the original image to the defined ROI
 	cv::Mat crop = img(roi);
@@ -63,18 +57,18 @@ crop (cv::Mat img, cv::Rect roi, int x, int y, int z)
 
 	// Put the cropped image onto the background
 	cv::Rect overflow(flow.left, flow.top, crop.size().width, crop.size().height);
-	crop.copyTo(background(overflow));
+	crop.copyTo((*cropped_img)(overflow));
+}
 
+void
+save_image(cv::Mat img, int x, int y, int z)
+{
 	// Ensure that folder exist
 	std::string folderName = std::to_string(z) + "/" + std::to_string(x);
 	std::experimental::filesystem::create_directories(folderName);
 
 	// Write final image to disk
-	cv::imwrite(std::to_string(z) + "/" + std::to_string(x) + "/" + std::to_string(y) + ".png", background);
-
-	dlog("DONE");
-
-	i++;
+	cv::imwrite(std::to_string(z) + "/" + std::to_string(x) + "/" + std::to_string(y) + ".png", img);
 }
 
 int
@@ -108,7 +102,9 @@ main ()
 		printf("Cut column Z:%d, X:%d\n", 13, x);
 		for (int y = 2178; roi.y <= img.size().height; y++)
 		{
-			crop(img, roi, x, y, 13);
+			cv::Mat cropped_img(roi.width, roi.height, CV_8UC4, cv::Scalar(0, 0, 0, 0));
+			crop(img, roi, &cropped_img);
+			save_image(cropped_img, x, y, 13);
 			roi.y += roi.height;
 		}
 
