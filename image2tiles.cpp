28e2a5b56db8e5528a45ad2d6eeefb47fccae8e6
Hauke Stieler <mail@hauke-stieler.de>
2019-01-23 01:09:23 +0100

    Render all zoom levels above given one

--- image2tiles.cpp
+++ image2tiles.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <algorithm>
 #include <experimental/filesystem>
 
 #include <opencv2/opencv.hpp>
@@ -64,11 +65,11 @@ void
 save_image(cv::Mat img, int x, int y, int z)
 {
 	// Ensure that folder exist
-	std::string folderName = std::to_string(z) + "/" + std::to_string(x);
+	std::string folderName = "out/" + std::to_string(z) + "/" + std::to_string(x);
 	std::experimental::filesystem::create_directories(folderName);
 
 	// Write final image to disk
-	cv::imwrite(std::to_string(z) + "/" + std::to_string(x) + "/" + std::to_string(y) + ".png", img);
+	cv::imwrite("out/" + std::to_string(z) + "/" + std::to_string(x) + "/" + std::to_string(y) + ".png", img);
 }
 
 int
@@ -85,8 +86,7 @@ main ()
 
 	printf("Start cuttig it ...\n");
 
-	/* Set Region of Interest */
-
+	// Set Region of Interest
 	int zoom_level = 13;
 
 	int output_tile_size = 256;
@@ -103,25 +103,82 @@ main ()
 	int start_coord_x = 3638;
 	int start_coord_y = 2178;
 
-	for (int x = start_coord_x; roi.x <= img.size().width; x++)
+	for (int z = zoom_level; z >= 0; z--)
 	{
-		printf("Cut column Z:%d, X:%d\n", zoom_level, x);
+		dlog("y:%d, y:%d, w:%d, h:%d", roi.x, roi.y, roi.width, roi.height);
+		dlog("Top left tile: https://a.tile.openstreetmap.org/%d/%d/%d.png", z, start_coord_x, start_coord_y);
 
-		for (int y = start_coord_y; roi.y <= img.size().height; y++)
+		for (int x = start_coord_x; roi.x <= img.size().width; x++)
 		{
-			cv::Mat cropped_img(roi.width, roi.height, CV_8UC4, cv::Scalar(0, 0, 0, 0));
-			crop(img, roi, &cropped_img);
+			printf("Cut column Z:%d, X:%d\n", z, x);
 
-			resize(cropped_img, cropped_img, cv::Size(output_tile_size, output_tile_size), 0, 0, cv::INTER_LINEAR_EXACT);
+			for (int y = start_coord_y; roi.y <= img.size().height; y++)
+			{
+				cv::Mat cropped_img(roi.width, roi.height, CV_8UC4, cv::Scalar(0, 0, 0, 0));
+				crop(img, roi, &cropped_img);
 
-			save_image(cropped_img, x, y, zoom_level);
+				resize(cropped_img, cropped_img, cv::Size(output_tile_size, output_tile_size), 0, 0, cv::INTER_LINEAR_EXACT);
 
-			roi.y += roi.height;
+				save_image(cropped_img, x, y, z);
+
+				roi.y += roi.height;
+			}
+
+			roi.x += roi.width;
+			roi.y = first_tile_y;
 		}
 
-		roi.x += roi.width;
-		roi.y = first_tile_y;
-	}	
+		roi.x = first_tile_x;
+
+		/*
+		 * When the current coordinate is odd, the corner of the upper (upper
+		 * = one z layer less) tile is shifted
+		 * 
+		 *    |x:100   'x':101  |x:102
+		 *    |x':50   '        |x':51
+		 * ---+-----------------+---
+		 *    |        '        |
+		 *    |        '        |
+		 *    |        '        |
+		 *  - + - - - - - - - - + - 
+		 *    |        '        |
+		 *    |        '        |
+		 *    |        '        |
+		 * ---+-----------------+---
+		 *    |        ,        |
+		 * 
+		 * x is the position on e.g. zoom level 13 and x' would then be the
+		 * position on zoom level 12. If a feature is on tile, which has the
+		 * position x:101, it'll appear on the tile x':50 whose origin is
+		 * shifted to the left. This is only the case when the original tile
+		 * has an odd coordinate.
+		 */
+		if (coord_x % 2 != 0)
+		{
+			roi.x -= roi.width; 
+			start_coord_x--;
+		}
+		if (coord_y % 2 != 0)
+		{
+			roi.y -= roi.height; 
+			start_coord_y--;
+		}
+
+		resize(img, img, cv::Size(std::max(1, img.size().width / 2), std::max(1, img.size().height / 2)), 0, 0, cv::INTER_AREA);
+
+		// When the image is half as large, all distances and offsets have to
+		// be as well
+		roi.x /= 2;
+		roi.y /= 2;
+
+		// Because the loops are resetting the positions
+		first_tile_x = roi.x;
+		first_tile_y = roi.y;
+
+		// Actually update the x and y coordinates
+		start_coord_x /= 2;
+		start_coord_y /= 2;
+	}
 
 	printf("Done!\n");
 
