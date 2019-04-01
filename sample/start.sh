7a80b7ed20bc0b82b14c03ed3d60dc9b8fb6191c
Hauke Stieler <mail@hauke-stieler.de>
2019-04-02 00:43:03 +0200

    Just use one start.sh script for samples

--- sample/start.sh
+++ sample/start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+#PROFILE=sample.jpg
+PROFILE=sample_sketch.jpg
+
 set -e
 
 echo "First build 'image2tiles' in the dir above"
 echo
 
-../image2tiles --p1=722,-19.0649,290,64.0088 --p2=1360,-19.0182,421,64.0046 --file=sample.jpg --max-zoom-level=13 --debug
+../image2tiles --p1=722,-19.0649,290,64.0088 --p2=1360,-19.0182,421,64.0046 --file=$PROFILE --max-zoom-level=15 --debug
 
 echo
 echo "Just open your browser on the following address:"
